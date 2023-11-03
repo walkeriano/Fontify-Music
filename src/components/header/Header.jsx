32af1b9e16780a32415b85c455016a4d8554e7a0
Daniel Cueto <dcueto14.info@gmail.com>
2023-11-03 09:48:04 +0100

    Header?

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,29 @@
 import "./Header.css";
 import logo from "../../img/logo-frontify.svg";
+import cover from "../../img/cover.jpg";
 
-import SearchInput from "./SearchInput";
-
-export default function Header({handleInputSearch, handleSearch}) {
-
-
+export default function Header() {
   return (
-    <header className="mainHeader">
-      <img className="logo" src={logo} alt="logo" />
-      <SearchInput handleInputSearch={handleInputSearch} handleSearch={handleSearch} />
-    </header>
+    <section className="sec-catalogue">
+      <header className="tools">
+        <img className="logo" src={logo} alt="logo" />
+        <div className="search-head">
+          <input type="search" placeholder="Buscar" />
+          <button>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+      </header>
+      <section className="cover-bg">
+        <img src={cover} alt="img-cover" />
+        <div className="cont-name-album">
+          <h3>Dangerous</h3>
+          <p>Album Más Top - Michael Jackson</p>
+        </div>
+      </section>
+      <div >
+
+      </div>
+    </section>
   );
 }
