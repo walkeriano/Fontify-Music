63ea3e97a7917710391582033ffca1ed2ffd4c88
Matheus <matriblima97@gmail.com>
2023-11-03 10:02:08 +0100

    nada

---
+++ src/components/Header/LoginBtn.jsx
@@ -0,0 +1,8 @@
+import './LoginBtn.css';
+
+export default function LoginBtn(){
+
+  return (
+    <button className="loginBtn">Login</button>
+  )
+}
---
+++ src/components/Header/SearchInput.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from 'react';
+import './SearchInput.css';
+
+export default function SearchInput({handleInputSearch, handleSearch}){
+
+
+  return (
+    <div className="searchField">
+      <input type="search" placeholder="Buscar" onChange={handleInputSearch} onKeyDown={handleSearch}/>
+      <button onClick={handleSearch}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+  )
+
+}
