1955d22e3f1db6d2a45ff4ffce772d22696af660
Daniel Cueto <dcueto14.info@gmail.com>
2023-11-06 11:32:10 +0100

    merge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,57 +3,59 @@ import logo from "../../img/logo-frontify.svg";
 import SearchInput from "./SearchInput";
 import { useState } from "react";
 
-export default function Header({sendData, sendIsDoingSearch, token}) {
-  const [isDoingSearch, setIsDoingSearch] = useState(false);
+export default function Header({ sendData, sendIsDoingSearch, token }) {
   const [inputValue, setInputValue] = useState(null);
-  function handleInputSearch(input) {
-    setInputValue(input.target.value.toLowerCase());
-  }
   function handleSearch(e) {
-    if (e.key == 'Enter') {
+    if (e.key == "Enter") {
       search();
-    } else if(e.type == 'click') {
+    } else if (e.type == "click") {
       search();
     }
   }
-
-  async function search() {
-    setIsDoingSearch(true);
-    sendIsDoingSearch(true);
-    console.log('Search for ' + inputValue);
-
-    // Get request using search to get Album ID
-
-    try{
-        // setIsLoading(true);
-        let options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
-        }
-
-        let requestAlbum = await fetch('https://api.spotify.com/v1/search?q=' + inputValue + '&type=album', options);
-        let responseAlbum = await requestAlbum.json();
-
-        if (requestAlbum.ok) {
-            console.log(responseAlbum, ' ALBUM FETCH');
-           // setSearchResults(responseAlbum.albums.items);
-            sendData(responseAlbum.albums.items);
-        }
-    } catch(error){
-        console.log(error);
-    } finally{
-        //setIsDoingSearch(false);
+  function handleInputValue(input) {
+    setInputValue(input.target.value.toLowerCase());
+    if (input.target.value.trim() == "") {
+      sendIsDoingSearch(false);
     }
-}
+  }
+  async function search() {
+    sendIsDoingSearch(true);
+    console.log("Search for " + inputValue);
+
+    try {
+      // setIsLoading(true);
+      let options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+
+      let requestAlbum = await fetch(
+        "https://api.spotify.com/v1/search?q=" + inputValue + "&type=album",
+        options
+      );
+      let responseAlbum = await requestAlbum.json();
+
+      if (requestAlbum.ok) {
+        console.log(responseAlbum, " ALBUM FETCH");
+        // setSearchResults(responseAlbum.albums.items);
+        sendData(responseAlbum.albums.items);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  }
 
   return (
     <header className="tools">
       <img className="logo" src={logo} alt="logo" />
-      <SearchInput handleInputSearch={handleInputSearch} handleSearch={handleSearch}/>
+      <SearchInput
+        handleInputSearch={handleInputValue}
+        handleSearch={handleSearch}
+      />
     </header>
   );
 }
-
