7b981dd9a757bd0462b055ff3b50c6e198a7cb27
MarinaRull <mrullproduction@gmail.com>
2023-11-09 14:27:07 +0100

    album page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,17 @@
 import "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage  from "./pages/Home/HomePage";
-import LoginPage  from "./pages/Login/LoginPage";
+import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
 import AlbumPage from "./pages/Album/AlbumPage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
 
-
 export function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/login-page" element={<LoginPage/>} />
-            <Route path="/dashboard" element={<DashboardPage/>} />
-            <Route path="/album/:id" element={<AlbumPage/>} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login-page" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/album/:id" element={<AlbumPage />} />
+    </Routes>
+  );
 }
